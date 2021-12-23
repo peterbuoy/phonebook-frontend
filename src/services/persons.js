f49712cd75f00f0399997701e7cb068d803d28d5
peterbuoy <peterbuoy@gmail.com>
2021-12-23 00:09:58 -0800

    change baseurl to relative path and add localhost as proxy arg in package.json

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "https://pure-lowlands-40590.herokuapp.com/api/persons";
+const baseUrl = "api/persons";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
